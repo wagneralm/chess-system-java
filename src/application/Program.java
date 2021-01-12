c81f59def14397759555d797c0e72a83284fe87a
Wagner de Almeida <wagneer.alm@gmail.com>
2021-01-11 22:59:10 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!! ");
+		Position pos = new Position(3, 6);
+		System.out.println(pos);
 	}
 
 }
